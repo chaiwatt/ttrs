eeb384493779b5cf5f0992026348210420ceaf6a
chaiwatt <chaiwat_thaweechan@yahoo.co.th>
2020-03-04 08:40:30 +0700

    remove unique users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->unsignedBigInteger('prefix_id')->default(1);
             $table->string('name');
             $table->string('lastname')->nullable();
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('picture')->nullable();
